8e39c7f3e47317445256c73c6f79946b9228cb36
zhijun.li <zhijun.li@timanetworks.com>
2020-04-24 15:24:30 +0800

    几个顺序表问题

--- app/src/main/java/xx/love/datastructure/MainActivity.kt
+++ app/src/main/java/xx/love/datastructure/MainActivity.kt
@@ -2,6 +2,8 @@ package xx.love.datastructure
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.util.Log
 import android.view.animation.Animation
 import android.view.animation.AnimationUtils
@@ -28,5 +30,15 @@ class MainActivity : AppCompatActivity() {
             twoNumbers = twoNumbers.next
             println(twoNumbers?.value)
         }
+        val handler= Handler( object : Handler.Callback{
+            override fun handleMessage(msg: Message?): Boolean {
+                if (msg?.what==0){
+                    println("ok")
+                }
+                return true
+            }
+        } )
+        handler.sendEmptyMessage(1);
+        handler.sendMessageAtTime(handler.obtainMessage(),1000)
     }
 }
